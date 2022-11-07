eb19ef3ae300684335261fe32cea91b524734198
Léo Xere <williams201012x@hotmail.com>
2022-11-07 22:20:46 +0100

    add passwd

--- tuto/commands.py
+++ tuto/commands.py
@@ -46,3 +46,29 @@ def loaddb(filename):
         db.session.add(book)
     # on sauvegarde les livres en BD
     db.session.commit()
+
+@app.cli.command()
+@click.argument('username')
+@click.argument('password')
+def newuser(username , password):
+    '''Adds a new user.'''
+    from .models import User
+    from hashlib import sha256
+    m = sha256()
+    m.update(password.encode())
+    u = User(username=username , password=m.hexdigest())
+    db.session.add(u)
+    db.session.commit()
+
+@app.cli.command()
+@click.argument('username')
+@click.argument('password')
+def passwd(username , password):
+    '''Change password for user.'''
+    from .models import User
+    from hashlib import sha256
+    m = sha256()
+    m.update(password.encode())
+    u = User.query.get(username)
+    u.password = m.hexdigest()
+    db.session.commit()
