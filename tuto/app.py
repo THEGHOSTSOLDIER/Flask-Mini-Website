160403e4af0e931e51929b0dbf7fb4f13fcbe176
Léo Xere <williams201012x@hotmail.com>
2022-11-09 08:57:12 +0100

    update uuid

--- tuto/app.py
+++ tuto/app.py
@@ -19,7 +19,7 @@ from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'
 +mkpath('../books.db'))
-app.config['SECRET_KEY'] = "1f133d3c-998f-4e70-8313-905d5169d5fe"
+app.config['SECRET_KEY'] = "334b0166-1e09-4e83-9593-26cac9b07096"
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
